22ca9c71f9a5b84a23b474b4419fa3aa0b5cfcc0
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-02-21 00:44:48 +0000

    Aplica padrões pessoais de indentação, pontuação e linhas em branco.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular17-app';
+  public title = 'angular17-app';
 }
